9dafdcf74f4325b775c27cade73da301469ec510
Ka Wing Fong <fongkawing@gmail.com>
2017-09-07 19:51:15 -0500

    added info

--- app/actions/recipes.js
+++ app/actions/recipes.js
@@ -1,4 +1,28 @@
 import * as types from './types'
+import Api from '../lib/api'
+
+export function fetchRecipes(ingredients){
+    return (dispatch, getState) => {
+        // console.log(getState);
+        const params = [
+            `ingredients=${encodeURIComponent(ingredients)}`,
+            'p=1'
+        ].join('&')
+        
+        return Api.get(`/recipes/findByIngredients?${params}`).then(resp=>{
+            dispatch(setSearchRecipes({recipes: resp}));
+        }).catch((ex)=>{
+            console.log(ex);
+        });
+    }
+}
+
+export function setSearchRecipes({ recipes }){
+    return {
+        type: types.SET_SEARCHED_RECIPES,
+        recipes
+    }
+}
 
 export function add_Recipe(){
     return {
--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react'
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
-import { ReactNative} from 'react-native' 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { ActionCreators } from '../actions'
+import Home from './Home'
 
 class AppContainer extends React.Component {
 
     constructor(props){
         super(props);
         this.state = {recipeCount: 0};
-    }
+    }    
 
     addRecipe(){
         this.props.add_Recipe()
@@ -18,14 +17,7 @@ class AppContainer extends React.Component {
 
     render() {
         return (
-        <View>
-            <Text style = {{marginTop: 20}}>
-                I am App Container! Recipe Count: {this.props.recipeCount}
-            </Text>
-            <TouchableHighlight onPress = {() => {this.addRecipe()}}>
-                <Text>Add recipe</Text>
-            </TouchableHighlight>
-        </View>
+            <Home {...this.props}/>
         );
     }
 }
